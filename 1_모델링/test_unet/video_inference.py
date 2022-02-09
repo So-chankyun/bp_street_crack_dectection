0abc28ae45442ded33bb69a959f723a550b26202
Jongchan Yun <yunjch21@gmail.com>
2022-02-09 16:25:51 +0900

    video inference to make .py file : make ratio navigator

--- 1_모델링/test_unet/video_inference.py
+++ 1_모델링/test_unet/video_inference.py
@@ -7,6 +7,8 @@ from PIL import Image
 from unet import UNet
 
 """
+--- 모델 아이디어 및 테스트는 test_unet/mak_video_detect.ipynb 에서 확인할 수 있음 ---
+
 전처리 로직이 다르다면, preprocess 메서드 수정 필요
 """
 def preprocess(img, scale):
@@ -34,6 +36,21 @@ def merge_img(frame, pred):
     return add_mask
 
 def get_args():
+    
+    """
+    parser의 argument 정의
+    
+        --width : 이미지 너비
+        --height : 이미지 높이
+        --v_number : 비디오 번호 - input video 파일 명 예) sample_video9.mp4
+        --frame : output video 저장 시 프레임 수
+        --model_dir : 모델이 있는 directory 이름 (main code 참조)
+        --m_cam : 내장 카메라 사용 여부 (활성화 시 input video 대신 내장 카메라 작동)
+        --save : 저장 여부 (활성화 시 save 경로로 저장 - save 경로는 main에서 정의)
+        --frame_thred : Crack 비율의 경계 값 (예 : 1.0이면 1.0을 초과하는 frame의 ratio를 red로 표시)
+        
+    """
+    
     parser = argparse.ArgumentParser(description='Video Inference Learning Parameters')
     parser.add_argument('--width', '-W', type=int, default=640, help='Model Input Width')
     parser.add_argument('--height', '-H', type=int, default=360, help='Model Input Height')
@@ -41,7 +58,8 @@ def get_args():
     parser.add_argument('--frame', '-fps', type=float, default=30.0, help='Output video Frame')
     parser.add_argument('--model_dir', '-md', type=str, default='UNet_b2th5dn200k', help='Input checkpoint Directory')
     parser.add_argument('--m_cam', action='store_true', default=False, help='Use Device Camera')
-    parser.add_argument('--save', action='save_option', default=False, help='Save Video option')
+    parser.add_argument('--save', action='store_true', default=False, help='Save Video option')
+    parser.add_argument('--frame_thred', '-fth', type=float, default=100, help='Frame Threshold Ratio')
     return parser.parse_args()
 
 if __name__ == '__main__':
@@ -75,12 +93,17 @@ if __name__ == '__main__':
             print("프레임을 수신할 수 없습니다. 종료 중 ...")
             break
 
-        convert_img = merge_img(frame, pred_frame(frame, model, DEVICE))
-        cv2.imshow("Test Vidoe Crack Detect", convert_img)
+        pred_img = pred_frame(frame, model, DEVICE)
+        convert_img = merge_img(frame, pred_img)
+        CRPF = np.round((pred_img.reshape(-1).sum()/(args.width*args.height))*100, 2)
         
-        if args.save:
-            out.write(convert_img)
+        font=cv2.FONT_HERSHEY_SIMPLEX
+        color = (50,50,165) if CRPF > args.frame_thred else (50,165,50)
+        CRPF_img = cv2.putText(convert_img, 'Crack Ratio : {:.2f} {}'.format(CRPF, "%"), (5, 20), font, .6, color, 2)
 
+        cv2.imshow("Test Vidoe Crack Detect", CRPF_img)
+        if args.save:
+            out.write(CRPF_img)
 
     capture.release()
     if args.save:
